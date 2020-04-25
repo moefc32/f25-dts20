4b8dfcba76aaf4e9b7ac8d6b3ec36e3c276105c0
Faizal Chan <moefc32@gmail.com>
2020-04-26 05:32:37 +0700

    update

---
+++ resources/script.js
@@ -0,0 +1,29 @@
+function scrollLanding() {
+  document.querySelector('#landing').scrollIntoView({
+    behavior: 'smooth'
+  });
+}
+
+function scrollLesson() {
+  document.querySelector('#lesson').scrollIntoView({
+    behavior: 'smooth'
+  });
+}
+
+function scrollProfile() {
+  document.querySelector('#profile').scrollIntoView({
+    behavior: 'smooth'
+  });
+}
+
+function scrollPortfolio() {
+  document.querySelector('#portfolio').scrollIntoView({
+    behavior: 'smooth'
+  });
+}
+
+function scrollOrganizer() {
+  document.querySelector('#organizer').scrollIntoView({
+    behavior: 'smooth'
+  });
+}
