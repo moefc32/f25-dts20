21409c745c290ea12e97a6b87d73e6668fa1a978
Faizal Chan <moefc32@gmail.com>
2020-04-26 19:21:01 +0700

    update

--- resources/script.js
+++ resources/script.js
@@ -1,43 +1,13 @@
-var mybutton = document.getElementById("to-top");
-
-window.onscroll = function() {
-  scrollFunction()
-};
-
-function scrollFunction() {
+window.onscroll = function scrollFunction() {
+  var mybutton = document.getElementById('to-top');
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    mybutton.style.right = "30px";
+    mybutton.style.right = '30px';
   } else {
-    mybutton.style.right = "-70px";
+    mybutton.style.right = '-70px';
   }
 }
 
-function scrollLanding() {
-  document.querySelector('#landing').scrollIntoView({
-    behavior: 'smooth'
-  });
-}
-
-function scrollLesson() {
-  document.querySelector('#lesson').scrollIntoView({
-    behavior: 'smooth'
-  });
-}
-
-function scrollProfile() {
-  document.querySelector('#profile').scrollIntoView({
-    behavior: 'smooth'
-  });
-}
-
-function scrollPortfolio() {
-  document.querySelector('#portfolio').scrollIntoView({
-    behavior: 'smooth'
-  });
-}
-
-function scrollOrganizer() {
-  document.querySelector('#organizer').scrollIntoView({
-    behavior: 'smooth'
-  });
+const scrollLink = (id)=> {
+  var a = document.getElementById(id);
+  a.scrollIntoView({behavior: 'smooth'});
 }
