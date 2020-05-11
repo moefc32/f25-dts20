201c0e297bc9a78d71b06daf5b8a5e7b571359e0
Faizal Chan <moefc32@gmail.com>
2020-05-11 11:42:13 +0700

    update

--- resources/calculator.js
+++ resources/calculator.js
@@ -1,7 +1,48 @@
-const number = (val)=> {
-  console.log(val);
+const calculatorScreen = document.querySelector('#display');
+const calculatorLog = document.querySelector('#log');
+
+let prevNumber;
+let calculationOperator;
+let currentNumber = '0';
+
+const updateScreen = (val)=> {
+  calculatorScreen.value = val;
 }
 
-const operation = (val)=> {
-  console.log(val);
+const updateLog = (val)=> {
+  calculatorLog.value += val + '\n';
+}
+
+const number = (val)=> {
+  if (currentNumber.length < 10) {
+    if (currentNumber === '0') {
+      if (val === '.') {
+        currentNumber = '0.';
+        updateScreen(currentNumber);
+        updateLog(currentNumber);
+      } else {
+        currentNumber = val;
+        updateScreen(currentNumber);
+        updateLog(currentNumber);
+      }
+    } else {
+      currentNumber += val;
+      updateScreen(currentNumber);
+      updateLog(currentNumber);
+    }
+  }
+}
+
+const operator = (val)=> {
+  if (val === 'clear') {
+    currentNumber = '0';
+    updateScreen(currentNumber);
+    updateLog('all clear');
+  } else {
+    prevNumber = currentNumber;
+    calculationOperator = val;
+    currentNumber = "0";
+    updateScreen(currentNumber);
+    updateLog(currentNumber);
+  }
 }
